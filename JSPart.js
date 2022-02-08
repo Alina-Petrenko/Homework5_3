1a7ac044bf101bbd33e92737e70fcfd5694081bd
Alina Petrenko <petrenkoaalina@gmail.com>
2022-02-08 11:47:17 +0200

    fix

--- JSPart.js
+++ JSPart.js
@@ -6,7 +6,7 @@ function counter (value)
         counter = function() 
         {     
             let arg;      
-            if (arguments.length > 0)
+            if (arguments[0])
                 {
                     arg = arguments[0];
                     return newValue = newValue + arg;
@@ -18,39 +18,39 @@ function counter (value)
         };
         return counter();
     }
-    console.log(counter(1));
-    console.log(counter(2)); 
-    console.log(counter(7));
-    console.log(counter(10));  
+console.log(counter(1));
+console.log(counter(2)); 
+console.log(counter(7));
+console.log(counter(10));  
 
 console.log("Second Part JS Homework")
-
 function getUpdatedArr() 
-    {    
-        let arg = []; 
-        return getUpdatedArr = function() 
-            {   
-              if (arguments.length > 0)
-              {
-                for (let i of arguments)      
-                {
-                        arg.push(i)
-                        return arg;  
-                }   
-              }
-              else
-              {
-                arg = [];
+{    
+    let arg = []; 
+    return getUpdatedArr = function() 
+    {   
+                      
+        if (arguments.length>0)
+        { 
+            for (let i of arguments)      
+            {    
+                arg.push(i)
                 return arg;
-              } 
-                
-            };
-    }
-    let a = getUpdatedArr();
-         
-    console.log(a(3));
-    console.log(a(5));
-    console.log(a(6));
-    console.log(a());
-    console.log(a(2));
+            }
+        }
+        else
+        {
+            i = 0;
+            return arg = [];
+        }     
+                          
+    };
+}
+
+let a = getUpdatedArr();         
+console.log(a(3));
+console.log(a(5));
+console.log(a(6));
+console.log(a());
+console.log(a(2));
 
